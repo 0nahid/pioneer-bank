9460b0ba3dc083538b40c3015743ef7788725711
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-01-26 15:17:34 +0600

    catch user input

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
+// Login button handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
+});
+
+//  Deposit button handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+    const depositAmount = parseFloat(document.getElementById("depositAmount").value);
+    
 });

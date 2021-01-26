c6bbc55ed148f0891c6c1f28212afc364d6355ae
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-01-26 18:27:52 +0600

    deposit button works

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 // Login button handler
-const loginBtn = document.getElementById("login");
-loginBtn.addEventListener("click", function () {
-    const loginArea = document.getElementById("login-area");
-    loginArea.style.display = "none";
-    const transactionArea = document.getElementById("transaction-area");
-    transactionArea.style.display = "block";
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function () {
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
 });
 
 //  Deposit button handler
-const depositBtn = document.getElementById("addDeposit");
-depositBtn.addEventListener("click", function () {
-    const depositAmount = parseFloat(document.getElementById("depositAmount").value);
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function () {
+    const depositAmount = parseFloat(document.getElementById('depositAmount').value);
+
+    const currentDeposit = parseFloat(document.getElementById('currentDeposit').innerText);
+    const totalDeposit = depositAmount + currentDeposit;
+    console.log(totalDeposit);
     
+    document.getElementById('currentDeposit').innerText = totalDeposit;
 });
